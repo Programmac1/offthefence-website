7c8b3974c858bd21ab65360f6a1ad79fb0b3dfd5
Programmac1 <appledan616@gmail.com>
2023-11-15 16:31:32 +1100

    changes to otf-episodes
    added colour change effect of navbar on episodes page using JS
    added audio to episodes page and rearranged episode title text

--- scripts/index.js
+++ scripts/index.js
@@ -4,4 +4,16 @@ const mobile_menu = document.querySelector('.mobile-nav');
 menu_button.addEventListener('click', function (){
     menu_button.classList.toggle('is-active');
     mobile_menu.classList.toggle('is-active');
-})
+});
+
+/*document.addEventListener("scroll", function () {
+    var scrollPosition = window.scrollY;
+
+    if (scrollPosition > 100) {
+        document.querySelector("navbar").style.backgroundColour = rgb(56, 182, 255, 0.2);
+    }
+    else {
+        document.querySelector("navbar").style.backgroundColour =  rgb(56, 182, 255);
+    }
+});*/
+
---
+++ scripts/otf-podcasts.js
@@ -0,0 +1,29 @@
+const menu_button = document.querySelector('.hamburger');
+const mobile_menu = document.querySelector('.mobile-nav');
+
+menu_button.addEventListener('click', function (){
+    menu_button.classList.toggle('is-active');
+    mobile_menu.classList.toggle('is-active');
+});
+
+//
+
+document.addEventListener('scroll', function () {
+    const scrollPosition = window.scrollY;
+    const navbar = document.querySelector('.navbar');
+
+    if(scrollPosition > 400) {
+        navbar.classList.add('scrolled');
+    }else {
+        navbar.classList.remove('scrolled');
+    }
+})
+
+/* ChatGPT: In this corrected code:
+
+The selector is updated to use .navbar to match the class name of your navigation bar.
+The classList.add and classList.remove methods are used to add and remove the scrolled class, respectively.
+Make sure to replace the existing content of your otf-podcasts.js file with this corrected code. After making this change, your navigation bar should transition as you scroll down the page.*/
+
+//
+
