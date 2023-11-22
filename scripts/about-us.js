945983c0dfed7f1606e353583893f4fa61fbfc7c
Programmac1 <appledan616@gmail.com>
2023-11-22 15:48:19 +1100

    image not loading to pages
    changed image type tag to CAPS on VSC code as github pages is case sensitive (vsc isnt) and for some reason this image has saved with a caps image type tag so guthub pages couldnt locate the file

--- scripts/about-us.js
+++ scripts/about-us.js
@@ -51,7 +51,7 @@ function updateImageSrc() {
     const windowWidth = window.innerWidth;
 
     if(windowWidth < 767) {
-        backgImage.src = 'images/blue-mountains-fence-mobile.jpg';
+        backgImage.src = 'images/blue-mountains-fence-mobile.JPG';
     }else {
         backgImage.src = 'images/blue-mountains-resized-2.jpg';
     }
