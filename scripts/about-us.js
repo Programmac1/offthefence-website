ad6a5dac6fdab4b755f8623c7c5a00cb1d07c725
Programmac1 <appledan616@gmail.com>
2023-11-22 14:48:46 +1100

    fixed backg image issue- about-us.html
    in mobile view- the background image was not changing- this was because the JS query was set to listen for 'resize' but navigating from the hamburger menu we were not resizing the window to trigger the JS function.
    
    wrote new JS query to fix this

--- scripts/about-us.js
+++ scripts/about-us.js
@@ -21,14 +21,7 @@ menu_button.addEventListener('click', function (){
     }
 });*/
 
-window.addEventListener('resize', () => {
-    const backgImage = document.getElementById('background');
-    if(window.innerWidth < 767) {
-        backgImage.src = 'images/blue-mountains-fence-mobile.jpg';
-    }else{
-        backgImage.src = 'images/blue-mountains-resized-2.jpg'
-    }
-});
+
 
 /* ChatGPT: In this corrected code:
 
@@ -37,4 +30,38 @@ The classList.add and classList.remove methods are used to add and remove the sc
 Make sure to replace the existing content of your otf-podcasts.js file with this corrected code. After making this change, your navigation bar should transition as you scroll down the page.*/
 
 //
+
+
+/*window.addEventListener('resize', () => {
+    const backgImage = document.getElementById('background');
+    if(window.innerWidth < 767) {
+        backgImage.src = 'images/blue-mountains-fence-mobile.jpg';
+    }else{
+        backgImage.src = 'images/blue-mountains-resized-2.jpg'
+    }
+});*/
+
+//The above code doesnt work for mobile as it is listening for us resizing the screen but we arent- we are just navigating straight to the about us page. This is why the background image changes when we resize the window to mobile size but doesnt change when we navigate to about-us.html from the hamburger menu
+
+//
+
+const backgImage = document.getElementById('background');
+
+function updateImageSrc() {
+    const windowWidth = window.innerWidth;
+
+    if(windowWidth < 767) {
+        backgImage.src = 'images/blue-mountains-fence-mobile.jpg';
+    }else {
+        backgImage.src = 'images/blue-mountains-resized-2.jpg';
+    }
+}
+
+updateImageSrc();
+
+window.addEventListener('resize', updateImageSrc);
+
+
+
+
 
